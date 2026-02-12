360561d1fbf67eb024286a95aa4f5d1187b2a763
unknown <email_cua_ban@example.com>
2026-02-12 14:45:09 +0700

    feat: Add Floating Messenger Button

--- components/FacebookMsg.js
+++ components/FacebookMsg.js
@@ -1,37 +1,32 @@
 'use client'
 
-import { useEffect } from 'react'
-import { usePathname } from 'next/navigation'
+import React from 'react'
+import styles from './FacebookMsg.module.css'
 
 export default function FacebookMsg() {
-    const pathname = usePathname()
-
-    useEffect(() => {
-        // Initialize Facebook SDK
-        var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "61587738368961");
-        chatbox.setAttribute("attribution", "biz_inbox");
-
-        window.fbAsyncInit = function () {
-            FB.init({
-                xfbml: true,
-                version: 'v18.0'
-            });
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    }, [pathname])
+    const pageId = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID || '953279121207440';
 
     return (
-        <>
-            <div id="fb-root"></div>
-            <div id="fb-customer-chat" className="fb-customerchat"></div>
-        </>
+        <a
+            href={`https://m.me/${pageId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.container}
+            aria-label="Chat with us on Facebook"
+        >
+            {/* Messenger Icon SVG */}
+            <svg
+                viewBox="0 0 28 28"
+                fill="white"
+                className={styles.icon}
+            >
+                <path d="M14 0C6.268 0 0 6.007 0 13.414c0 4.02 1.838 7.632 4.8 10.024v5.185c0 .546.666.822 1.054.437l3.228-3.193a14.73 14.73 0 004.918.847c7.732 0 14-6.007 14-13.414S21.732 0 14 0zm1.745 18.056l-3.328-3.55-6.502 3.55 7.155-7.595 3.328 3.55 6.502-3.55-7.155 7.595z" />
+            </svg>
+
+            {/* Tooltip text */}
+            <span className={styles.tooltip}>
+                Chat với chúng tôi
+            </span>
+        </a>
     )
 }
